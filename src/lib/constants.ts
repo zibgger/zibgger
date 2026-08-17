1a199f5d9f9e95345f79bf8f5d836b67171819d9
zibgger <3420252006@qq.com>
2026-08-17 20:14:56 +0800

    Update constants.ts

--- src/lib/constants.ts
+++ src/lib/constants.ts
@@ -1,11 +1,11 @@
 export const STRAPI_BASE_URL = 'http://localhost:1337';
-export const SITE_NAME = 'Qian\'s Studio';
-export const SITE_DESCRIPTION = '探索设计、技术与生活的交点';
+export const SITE_NAME = 'Zhang\'s Studio';
+export const SITE_DESCRIPTION = '程序员';
 
 export const CATEGORY_LABELS: Record<string, string> = {
   web: 'Web 开发',
   design: '设计',
-  photo: '摄影',
+  photo: '小程序',
   other: '其他',
 };
 
